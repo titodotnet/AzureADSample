fcf4ebece28c3687c6cfb0841ba5011d38b2af3b
Tito Dakshinamoorthy <titodakshinamoorthy@hotmail.com>
2015-12-21 15:23:14 +0530

    AzureAD WSFed implementation

---
+++ WSFedWebApp1/App_Start/Startup.Auth.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.WsFederation;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WSFedWebApp1
+{
+    public partial class Startup
+    {
+        //
+        // The Client ID is used by the application to uniquely identify itself to Azure AD.
+        // The Metadata Address is used by the application to retrieve the signing keys used by Azure AD.
+        // The AAD Instance is the instance of Azure, for example public Azure or Azure China.
+        // The Authority is the sign-in URL of the tenant.
+        // The Post Logout Redirect Uri is the URL where the user will be redirected after they sign out.
+        //
+        private static string realm = ConfigurationManager.AppSettings["ida:Wtrealm"];
+        private static string aadInstance = ConfigurationManager.AppSettings["ida:AADInstance"];
+        private static string tenant = ConfigurationManager.AppSettings["ida:Tenant"];
+        private static string metadata = string.Format("{0}/{1}/federationmetadata/2007-06/federationmetadata.xml", aadInstance, tenant);
+
+
+        string authority = String.Format(CultureInfo.InvariantCulture, aadInstance, tenant);
+
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+
+            app.UseWsFederationAuthentication(
+                new WsFederationAuthenticationOptions
+                {
+                    Wtrealm = realm,
+                    MetadataAddress = metadata,
+                    Notifications = new WsFederationAuthenticationNotifications
+                    {
+                        AuthenticationFailed = context =>
+                        {
+                            context.HandleResponse();
+                            context.Response.Redirect("Home/Error?message=" + context.Exception.Message);
+                            return Task.FromResult(0);
+                        }
+                    }
+                });
+        }
+    }
+}
---
+++ WSFedWebApp1/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.WsFederation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WSFedWebApp1.Controllers
+{
+    public class AccountController : Controller
+    {
+        public void SignIn()
+        {
+            // Send a WSFederation sign-in request.
+            if (!Request.IsAuthenticated)
+            {
+                HttpContext.GetOwinContext().Authentication.Challenge(new AuthenticationProperties { RedirectUri = "/" }, WsFederationAuthenticationDefaults.AuthenticationType);
+            }
+        }
+        public void SignOut()
+        {
+            // Send a WSFederation sign-out request.
+            HttpContext.GetOwinContext().Authentication.SignOut(
+                WsFederationAuthenticationDefaults.AuthenticationType, CookieAuthenticationDefaults.AuthenticationType);
+        }
+    }
+}
