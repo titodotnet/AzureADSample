cf2f02f3610760d2bf5c94141f0eb155fe01a3be
Tito Dakshinamoorthy <titodakshinamoorthy@hotmail.com>
2015-12-22 11:59:37 +0530

    glimpse aded

---
+++ AzureADWebApp1/Custom/ClaimsTransformer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+
+namespace AzureADWebApp1.Custom
+{
+    public class ClaimsTransformer : ClaimsAuthenticationManager
+    {
+        public override ClaimsPrincipal Authenticate(string resourceName, ClaimsPrincipal incomingPrincipal)
+        {
+            return base.Authenticate(resourceName, incomingPrincipal);
+        }
+    }
+}
